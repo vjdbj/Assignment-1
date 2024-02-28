da21097f3c6b477f2c2e92cc0619efd6c43b3bf0
vjdbj <83549768+vjdbj@users.noreply.github.com>
2024-02-28 23:37:56 +0530

    Update test_case.py

--- tests/test_case.py
+++ tests/test_case.py
@@ -1,9 +1,9 @@
 import python_code
 
 def test_add():
-    assert your_python_code.add(1, 2) == 3
-    assert your_python_code.add(5, 7) == 12
+    assert python_code.add(1, 2) == 3
+    assert python_code.add(5, 7) == 12
 
 def test_subtract():
-    assert your_python_code.subtract(5, 2) == 3
-    assert your_python_code.subtract(10, 7) == 3
+    assert python_code.subtract(5, 2) == 3
+    assert python_code.subtract(10, 7) == 3
